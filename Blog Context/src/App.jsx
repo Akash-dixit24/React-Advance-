5f49202a5652c08082e0cb1f9c0568ee72d4a437
Akash <akashdixit2301@gmail.com>
2024-07-28 17:06:06 +0530

    class-2

--- Blog Context/src/App.jsx
+++ Blog Context/src/App.jsx
@@ -1,43 +1,23 @@
-import "./App.css";
-import React, { useContext, useEffect } from "react";
-import Home from "./Pages/Home";
-import BlogPage from "./Pages/BlogPage";
-import TagPage from "./Pages/TagPage";
-import CategoryPage from "./Pages/CategoryPage";
-import { AppContext } from "./Context/AppContext";
-import { Routes, Route, useLocation, useSearchParams } from "react-router-dom";
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import Pagination from "./components/Pagination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+import "./index.css"
 
-const App = () => {
-  const { fetchBlogPosts } = useContext(AppContext);
-  const location = useLocation();
-  const [searchParams, setSearchParams] = useSearchParams();
+export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
 
   useEffect(() => {
-    const page = searchParams.get("page") ?? 1;
-
-    const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
-    const category = location.pathname.split("/").at(-1).replaceAll("-", " ");
-
-    if (location.pathname.includes("tags")) {
-      fetchBlogPosts(Number(page), tag);
-    }
-    else if (location.pathname.includes("categories")) {
-      fetchBlogPosts(Number(page), category);
-    }
-    else {
-      fetchBlogPosts(Number(page));
-    }
-
-  }, [location.pathname, location.search]);
+    fetchBlogPosts();
+  },[]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog/:blogId" element={<BlogPage />} />
-      <Route path="/tags/:tag" element={<TagPage />} />
-      <Route path="/categories/:category" element={<CategoryPage />} />
-    </Routes>
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+      <Header />
+      <Blogs />
+      <Pagination />
+    </div>
   );
-};
 
-export default App;
+}
