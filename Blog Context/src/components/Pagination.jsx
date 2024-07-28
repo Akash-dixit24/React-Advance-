1fed0264d471abe4e964b805dc688f82de83135d
Akash <akashdixit2301@gmail.com>
2024-07-28 21:58:10 +0530

    full blog

--- Blog Context/src/components/Pagination.jsx
+++ Blog Context/src/components/Pagination.jsx
@@ -1,41 +1,34 @@
-import React from 'react'
-import { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Pagination = () => {
-
-   const {page, handlePageChange, totalPages} = useContext(AppContext); 
-
+  const { page, totalPages, handlerPageChange } = useContext(AppContext);
   return (
-    <div className='w-full flex justify-center items-center border-2 fixed bottom-0 bg-white'>
-      <div className='flex justify-between w-11/12 max-w-[670px] py-2'>
-      <div className='flex gap-x-2'> 
-        
-        { page > 1 &&
-            (<button 
-            className='rounded-md border-2 px-4 py-1'
-            onClick={() => handlePageChange(page-1)}>
-                Previous
-            </button>)
-        }
+    <div className="  fixed bottom-0 bg-white py-2 border-t-2 border-t-gray-300 w-full inset-x-0">
+      <div className="flex gap-x-3 items-center w-11/12 max-w-2xl mx-auto">
+        {page > 1 && (
+          <button
+            className="rounded-md border-gray-300 border-2 px-4 py-1"
+            onClick={() => handlerPageChange(page - 1)}
+          >
+            Previous
+          </button>
+        )}
 
-        { page < totalPages && 
-                (<button 
-                className='rounded-md border-2 px-4 py-1'
-                onClick={() =>handlePageChange(page+1) }>
-                Next
-            </button>)
-        }
-
-      </div>
-       
-
-        <p className='font-bold text-sm'>
-            Page {page} of {totalPages}
+        {page < totalPages && (
+          <button
+            className="rounded-md border-gray-300 border-2 px-4 py-1"
+            onClick={() => handlerPageChange(page + 1)}
+          >
+            Next
+          </button>
+        )}
+        <p className="text-sm font-semibold ml-auto">
+          Page {page} of {totalPages}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
